1603b9a9e43e373835c3fd46075b339969db711b
rom <rom>
2008-10-02 17:56:52 +0000

    needed for GreenMonster

---
+++ adc18/ADC18_cf_commands.h
@@ -0,0 +1,16 @@
+/********************************************
+*  ADC18_cf_commands.h                     *
+*  header for ADC18_config,                *
+*     which provides codes for              *
+*     ADC18 commands through the IP socket *
+********************************************/
+
+#define ADC18_GET_NUMADC 1001
+#define ADC18_GET_CSR    1002
+#define ADC18_SET_CONV   1003
+#define ADC18_GET_LABEL  1004
+#define ADC18_SET_INT    1005
+#define ADC18_SET_PED    1006
+#define ADC18_GET_CONV    1007
+#define ADC18_GET_INT    1008
+#define ADC18_GET_PED    1009
