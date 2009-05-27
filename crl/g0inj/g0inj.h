b4107f7fa9dcd4591dbd32097f8ce3f7f3867e9f
rom <rom>
2009-05-27 15:48:24 +0000

    G0/Qweak injector crate readout software

---
+++ crl/g0inj/g0inj.h
@@ -0,0 +1,30 @@
+/* Header file for g0vme1 */
+/*
+ *  pking, 2005oct11:  Corrected the path for the g0vme1.flags file.
+ */
+
+#define INTERNAL_FLAGS "ffile=/home/gzero/qweak/coda25/crl/g0inj.flags,badc=0xC1,nadc=3,vqwkinternal=0"
+
+#define NUM_ADCS "nadc"
+#define BASE_ADC "badc"
+#define VQWK_SAMPLE_PERIOD  "vqwkperiod"
+#define VQWK_NUM_BLOCKS     "vqwkblocks"
+#define VQWK_SAMP_PER_BLOCK "vqwksamples"
+#define VQWK_GATE_DELAY     "vqwkdelay"
+#define VQWK_INT_GATE_FREQ  "vqwkgatefreq"
+#define VQWK_INTERNAL_MODE  "vqwkinternal"
+
+#include "usrstrutils.c"
+#include "go_info.c"
+
+#include "myevtypes.h"
+
+typedef unsigned int UINT32;
+
+// scan utilities 
+extern int getDataScan(int);
+extern int getCleanScan();
+extern int setDataScan(int,int);
+extern int setCleanScan(int);
+
+
